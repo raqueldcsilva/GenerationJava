fff9e5036e86fd067fe2c720a588acdd214cbb0a
Raqueldcsilva <raquelshdy@gmail.com>
2021-08-11 19:51:21 -0300

    Atividade de herança do dia 09/08/2021

---
+++ Cachorro.java
@@ -0,0 +1,16 @@
+package br.com.generation.herança;
+
+public class Cachorro extends Animal {
+	
+	public void somAnimal() {
+		
+		System.out.println( "O CAchorro está latindo");
+	}
+	
+	public void correr() {
+		
+		System.out.println("O Cachorro corre");
+		
+	}
+
+}
---
+++ Cavalo.java
@@ -0,0 +1,15 @@
+package br.com.generation.herança;
+
+public class Cavalo extends Cachorro {
+	
+	public void somAnimal() {
+		
+		System.out.println("O cavalo está relinchando");
+	}
+	
+	public void correr () {
+		
+		System.out.println("O cavalo está correndo");
+	}
+
+}
